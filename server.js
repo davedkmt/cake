48ec314a3665db96409966917575e91ac8ac284c
davemtadesse <31293749+davemtadesse@users.noreply.github.com>
2017-11-16 10:00:01 -0600

    adjusted

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
 
-var connection = mysql.createConnection({
+connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
